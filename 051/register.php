5e7ad917b5e4a95d8ccd4b55de6768dd2ef3e821
Naphaz <664230050@webmail.npru.ac.th>
2025-08-07 22:24:11 +0700

    ii
    iiii

--- 051/register.php
+++ 051/register.php
@@ -11,7 +11,6 @@
         $stmt = $conn->prepare($sql);
         $stmt->execute([$username, $fullname, $email, $hashedPassword]);
         }
-  
 ?>
 
 <!DOCTYPE html>
@@ -20,46 +19,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5" >
-        <h2>Register</h2>
-        <form action="register.php" method="post">
-            <div class="mb-3">
-                <label for="username" class="form-label">ชื่อผู้ใช้</label>
-                <input type="text" name="username" id="username" class="form-control" placeholder="ชื่อผู้ใช้" required>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header text-center bg-danger text-white">
+                        <h2>Register</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="register.php" method="post">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">ชื่อผู้ใช้</label>
+                                <input type="text" name="username" id="username" class="form-control" placeholder="ชื่อผู้ใช้" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="fullname" class="form-label">ชื่อ-สกุล</label>
+                                <input type="text" name="fullname" id="fullname" class="form-control" placeholder="ชื่อ-สกุล" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">อีเมล</label>
+                                <input type="email" name="email" id="email" class="form-control" placeholder="อีเมล" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">รหัสผ่าน</label>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="รหัสผ่าน" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="confirm_password" class="form-label">ยืนยันรหัสผ่าน</label>
+                                <input type="password" name="confirm_password" id="confirm_password" class="form-control" placeholder="ยืนยันรหัสผ่าน" required>
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-danger">สมัครสมาชิก</button>
+                                <a href="login.php" class="btn btn-link text-danger">เข้าสู่ระบบ</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="fullname" class="form-label">ชื่อ-สกุล</label>
-                <input type="text" name="fullname" id="fullname" class="form-control" placeholder="ชื่อ-สกุล" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">อีเมล</label>
-                <input type="text" name="email" id="email" class="form-control" placeholder="อีเมล" required>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">รหัสผ่าน</label>
-                <input type="text" name="password" id="password" class="form-control" placeholder="รหัสผ่าน" required>
-            </div>
-            <div class="mb-3">
-                <label for="confirm_password" class="form-label">ยืนยันรหัสผ่าน</label>
-                <input type="text" name="confirm_password" id="confirm_password" class="form-control" placeholder="ยืนยันรหัสผ่าน" required>
-            </div>
-            <div>
-                <button type="submit" class="btn btn-primary">สมัครสมาชิก</button>
-                <a href="login.php" class="btn btn-link">เข้าสู่ระบบ</a>
-            </div>
-        </form>
+        </div>
     </div>
-
-
-
-
-
-
-
-
-    <script scr="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
